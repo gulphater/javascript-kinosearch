0058c2ec5210a397e8cab23aad39c9e8dd23a853
Shohrux <dotanoobbich@mail.ru>
2019-07-02 20:51:48 +0500

    Working with XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -6,9 +6,23 @@ function getApi(e) {
   e.preventDefault();
   let searchText = searchInput.val();
   let url = `https://api.themoviedb.org/3/search/multi?api_key=547af96e462fbc7597135bd797803004&language=en-US&query=${searchText}`;
+  requestApi("GET", url);
 }
 
-function requestApi(url) {}
+function requestApi(method, url) {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.send();
+  xhr.addEventListener("readystatechange", function(e) {
+    if (xhr.readyState !== 4) {
+      return;
+    } else if (xhr.status !== 200) {
+      throw `Error: ${xhr.status}`;
+    }
+
+    console.log(JSON.parse(xhr.responseText));
+  });
+}
 
 // code
 searchForm.on("submit", getApi);
