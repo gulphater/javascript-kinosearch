e419eef10cd56ec7e24b66f054bb3e1e7d103d47
Shohrux <dotanoobbich@mail.ru>
2019-07-02 19:03:46 +0500

    Getting API for searching

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// variables
+const searchForm = $("[data-target=search-form]");
+const searchInput = $("[data-target=search-input]");
+// functions
+function getApi(e) {
+  e.preventDefault();
+  let searchText = searchInput.val();
+  let url = `https://api.themoviedb.org/3/search/multi?api_key=547af96e462fbc7597135bd797803004&language=en-US&query=${searchText}`;
+}
+// code
+searchForm.on("submit", getApi);
