f46145aa9068afe25ff93b516bb0a68c18fb4489
Shohrux <dotanoobbich@mail.ru>
2019-07-02 19:29:00 +0500

    Creating function requestApi()

--- js/script.js
+++ js/script.js
@@ -7,5 +7,8 @@ function getApi(e) {
   let searchText = searchInput.val();
   let url = `https://api.themoviedb.org/3/search/multi?api_key=547af96e462fbc7597135bd797803004&language=en-US&query=${searchText}`;
 }
+
+function requestApi(url) {}
+
 // code
 searchForm.on("submit", getApi);
