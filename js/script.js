93689791768079712c62041d893b3d02c762b251
Shohrux <dotanoobbich@mail.ru>
2019-07-02 22:36:56 +0500

    Content test

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // variables
 const searchForm = $("[data-target=search-form]");
 const searchInput = $("[data-target=search-input]");
+const movies = $("#movies");
 // functions
 function getApi(e) {
   e.preventDefault();
   let searchText = searchInput.val();
-  let url = `https://api.themoviedb.org/3/search/multi?api_key=547af96e462fbc7597135bd797803004&language=en-US&query=${searchText}`;
+  let url = `https://api.themoviedb.org/3/search/multi?api_key=547af96e462fbc7597135bd797803004&language=en_US%20en_US&query=${searchText}`;
   requestApi("GET", url);
 }
 
@@ -19,8 +20,16 @@ function requestApi(method, url) {
     } else if (xhr.status !== 200) {
       throw `Error: ${xhr.status}`;
     }
+    let output = JSON.parse(xhr.responseText);
 
-    console.log(JSON.parse(xhr.responseText));
+    let content = "";
+
+    output.results.forEach(item => {
+      let itemName = item.name || item.title;
+      content += `<div class="col-3 bg-primary"> ${itemName} </div>`;
+    });
+
+    movies.html(content);
   });
 }
 
